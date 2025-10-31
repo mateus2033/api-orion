6eb3f7a39a27c19b6a617df48c8046d90b894788
mateus2033 <mateus2033@outlook.com>
2025-10-30 23:20:31 -0300

    Ajustes do ambiente

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import { debug } from 'debug'
 import App from './app'
 
 const log = debug('feedback-api:server')
-App.listen(process.env.APP_PORT || 4000, () => log('Servidor rodando perfeitamente ...'))
+App.listen(process.env.APP_PORT || 3000, () => log('Servidor rodando perfeitamente ...'))
