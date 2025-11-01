4e74ecf3c9aaa9d991ea28ad68e09522b0804c2b
mateus2033 <mateus2033@outlook.com>
2025-11-01 14:01:55 -0300

    docs: update README to add developer tips

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,11 @@ import App from './app'
 
 const log = debug('feedback-api:server')
 App.listen(process.env.APP_PORT || 3000, () => log('Servidor rodando perfeitamente ...'))
+
+App.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+});
+
+// App.on('unhandledRejection', (reason, promise) => {
+//   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+// });
